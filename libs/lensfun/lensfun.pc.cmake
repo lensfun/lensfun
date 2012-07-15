94c2747abe65bc22a5cd71a62f3c6f89abde83b3
Sebastian Kraft <mail@sebastiankraft.net>
2012-07-15 13:52:29 +0000

    patch #3301, #3300: pkgconfig and LIB_SUFFIX support for CMake

---
+++ libs/lensfun/lensfun.pc.cmake
@@ -0,0 +1,13 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+bindir=@CMAKE_INSTALL_PREFIX@/@BINDIR@
+libdir=@CMAKE_INSTALL_PREFIX@/@LIBDIR@
+includedir=@CMAKE_INSTALL_PREFIX@/@INCLUDEDIR@
+datadir=@CMAKE_INSTALL_PREFIX@/@DATADIR@
+docdir=@CMAKE_INSTALL_PREFIX@/@DOCDIR@
+
+Name: lensfun
+Description: A photographic lens database and access library
+Version: @VERSION_MAJOR@.@VERSION_MINOR@.@VERSION_MICRO@.@VERSION_BUGFIX@
+Requires.private: glib-2.0
+Libs: -L${libdir} -llensfun
+Cflags: -I${includedir}
