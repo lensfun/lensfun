360fcf5ce197559e2103a9dddabaf43cc7078f15
Torsten Bronger <bronger@physik.rwth-aachen.de>
2013-07-26 22:13:53 +0200

    Added view with further calibration samples, good and bad ones.

--- calibration/urls.py
+++ calibration/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,5 +8,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns("calibration.views",
                        (r"^$", "upload"),
                        (r"^results/(?P<id_>.+)", "show_issues"),
+                       (r"^target_tips$", TemplateView.as_view(template_name="calibration/good_bad_ugly.html")),
                        (r"^thumbnails/(?P<id_>.+?)/(?P<hash_>[0-9a-f]+)", "thumbnail"),
 )
