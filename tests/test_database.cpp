850645d82355da8b1d6559ab8d82cccef9d8799e
Sebastian Kraft <mail@sebastiankraft.net>
2014-10-07 20:38:33 +0200

    Add some basic test routines to tests/ folder

---
+++ tests/test_database.cpp
@@ -0,0 +1,77 @@
+#include <glib.h>
+#include <locale.h>
+#include "lensfun.h"
+
+typedef struct {
+    lfDatabase* db;
+} lfFixture;
+
+
+void db_setup(lfFixture *lfFix, gconstpointer data)
+{
+    lfFix->db = lf_db_new ();
+    lfFix->db->Load();
+}
+
+
+void db_teardown(lfFixture *lfFix, gconstpointer data)
+{
+    lfFix->db->Destroy();
+}
+
+
+// test different lens search strings
+void test_DB_lens_search(lfFixture* lfFix, gconstpointer data)
+{
+    const lfLens **lenses = NULL;
+
+    lenses = lfFix->db->FindLenses (NULL, NULL, "pEntax 50-200 ED");
+    g_assert_nonnull(lenses);
+    g_assert_cmpstr(lenses[0]->Model, ==, "smc Pentax-DA 50-200mm f/4-5.6 DA ED");
+    lf_free (lenses);
+
+    lenses = lfFix->db->FindLenses (NULL, NULL, "smc Pentax-DA 50-200mm f/4-5.6 DA ED");
+    g_assert_nonnull(lenses);
+    g_assert_cmpstr(lenses[0]->Model, ==, "smc Pentax-DA 50-200mm f/4-5.6 DA ED");
+    lf_free (lenses);
+
+    lenses = lfFix->db->FindLenses (NULL, NULL, "PENTAX fa 28mm 2.8");
+    g_assert_nonnull(lenses);
+    g_assert_cmpstr(lenses[0]->Model, ==, "smc Pentax-FA 28mm f/2.8 AL");
+    lf_free (lenses);
+}
+
+// test different camera search strings
+void test_DB_cam_search(lfFixture* lfFix, gconstpointer data)
+{
+    const lfCamera **cameras = NULL;
+
+    cameras = lfFix->db->FindCamerasExt("pentax", "K100D");
+    g_assert_nonnull(cameras);
+    g_assert_cmpstr(cameras[0]->Model, ==, "Pentax K100D");
+    lf_free (cameras);
+
+    cameras = lfFix->db->FindCamerasExt(NULL, "K 100 D");
+    g_assert_nonnull(cameras);
+    g_assert_cmpstr(cameras[0]->Model, ==, "Pentax K100D");
+    lf_free (cameras);
+
+    cameras = lfFix->db->FindCamerasExt(NULL, "PentAX K100 D");
+    g_assert_nonnull(cameras);
+    g_assert_cmpstr(cameras[0]->Model, ==, "Pentax K100D");
+    lf_free (cameras);
+
+}
+
+int main (int argc, char **argv)
+{
+
+    setlocale (LC_ALL, "");
+
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add("/database/lens search", lfFixture, NULL, db_setup, test_DB_lens_search, db_teardown);
+    g_test_add("/database/camera search", lfFixture, NULL, db_setup, test_DB_cam_search, db_teardown);
+
+    return g_test_run();
+}
---
+++ tests/test_modifier.cpp
@@ -0,0 +1,115 @@
+#include <glib.h>
+#include <locale.h>
+#include <math.h>
+#include "lensfun.h"
+
+typedef struct {
+    size_t      img_width, img_height;
+    lfLens*     lens;
+    lfModifier* mod;
+} lfFixture;
+
+// setup a standard lens
+void mod_setup(lfFixture *lfFix, gconstpointer data)
+{
+    lfFix->lens             = new lfLens();
+    lfFix->lens->CropFactor = 1.0f;
+    lfFix->lens->Type       = LF_RECTILINEAR;
+
+    lfLensCalibDistortion lensCalibDist = {LF_DIST_MODEL_POLY3, 12.0f, {0.1,0.5,0.5}};
+    lfFix->lens->AddCalibDistortion(&lensCalibDist);
+
+    // width and height have to be odd, so we have a defined center pixel
+    lfFix->img_height = 301;
+    lfFix->img_width  = 301;
+
+    lfFix->mod = lfModifier::Create (lfFix->lens, 1.0f, lfFix->img_width, lfFix->img_height);
+}
+
+
+void mod_teardown(lfFixture *lfFix, gconstpointer data)
+{
+    lfFix->mod->Destroy();
+    lf_free(lfFix->lens);
+}
+
+// test to verifiy that projection center is image center
+void test_mod_projection_center(lfFixture* lfFix, gconstpointer data)
+{
+    float in[2]  = {0, 0};
+    float res[2] = {0, 0};
+
+    // check mapping of center to center
+    lfLensType geom_types [] = {LF_PANORAMIC, LF_EQUIRECTANGULAR , LF_FISHEYE, LF_FISHEYE_EQUISOLID, LF_FISHEYE_ORTHOGRAPHIC, LF_FISHEYE_THOBY, LF_UNKNOWN};
+    int i = 0;
+    while (geom_types[i]!=LF_UNKNOWN) {
+
+        lfFix->mod->Initialize (
+            lfFix->lens, LF_PF_U8, 12.0f,
+            6.7f, 2.0f, 1.0f, geom_types[i],
+            LF_MODIFY_GEOMETRY, false);
+
+        // check if center is not influenced
+        in[0] = (lfFix->img_width-1)/2;
+        in[1] = (lfFix->img_height-1)/2;
+        lfFix->mod->ApplyGeometryDistortion(in[0],in[1],1,1,res);
+        g_assert_cmpfloat(in[0],==,res[0]);
+        g_assert_cmpfloat(in[1],==,res[1]);
+        i++;
+    }
+}
+
+// check if output becomes NaN when entering image corners
+void test_mod_projection_borders(lfFixture* lfFix, gconstpointer data)
+{
+    float in[2]  = {lfFix->img_width, lfFix->img_height};
+    float in2[2] = {(lfFix->img_width-1)/2, (lfFix->img_height-1)/2};
+    float res[2] = {0, 0};
+
+    lfLensType geom_types [] = {LF_RECTILINEAR, LF_PANORAMIC, LF_EQUIRECTANGULAR , LF_FISHEYE_STEREOGRAPHIC, LF_FISHEYE, LF_FISHEYE_EQUISOLID, LF_FISHEYE_ORTHOGRAPHIC, LF_FISHEYE_THOBY, LF_UNKNOWN};
+    int  j  = 0;
+    while (geom_types[j]!=LF_UNKNOWN) {
+
+        lfFix->lens->Type = geom_types[j];
+
+        int  i  = 0;
+        while (geom_types[i]!=LF_UNKNOWN) {
+            lfFix->mod->Initialize (
+                lfFix->lens, LF_PF_U8, 12.0f,
+                6.7f, 2.0f, 1.0f, geom_types[i],
+                LF_MODIFY_GEOMETRY, false);
+
+            if (lfFix->mod->ApplyGeometryDistortion(0,0,1,1,res)) {
+                g_assert_false(isnan(res[0]));
+                g_assert_false(isnan(res[1]));
+            }
+
+            if (lfFix->mod->ApplyGeometryDistortion(in[0],in[1],1,1,res)) {
+                g_assert_false(isnan(res[0]));
+                g_assert_false(isnan(res[1]));
+            }
+
+            if (lfFix->mod->ApplyGeometryDistortion(in2[0],in2[1],1,1,res)) {
+                g_assert_false(isnan(res[0]));
+                g_assert_false(isnan(res[1]));
+            }
+
+            i++;
+        }
+        j++;
+    }
+}
+
+
+int main (int argc, char **argv)
+{
+
+    setlocale (LC_ALL, "");
+
+    g_test_init(&argc, &argv, NULL);
+
+    g_test_add("/modifier/projection center", lfFixture, NULL, mod_setup, test_mod_projection_center, mod_teardown);
+    g_test_add("/modifier/projection borders", lfFixture, NULL, mod_setup, test_mod_projection_borders, mod_teardown);
+
+    return g_test_run();
+}
