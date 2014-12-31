f18496d16d2b150da542c5981ff80685d4d39f8e
Sebastian Kraft <mail@sebastiankraft.net>
2014-12-31 12:00:19 +0100

    Python script for checking database integrity is now automatically called by CTest

--- tools/check_database/check_database.py
+++ tools/check_database/check_database.py
@@ -11,6 +11,9 @@ It must take a set of ElementTree roots.
 import glob, sys, os, inspect
 from xml.etree import ElementTree
 
+ERROR_FOUND = False
+
+
 def normalize_string(string):
     return " ".join(string.lower().split())
 
@@ -21,24 +24,28 @@ def name(element, tag_name):
 
 
 def check_primary_keys_uniqueness(roots):
+    global ERROR_FOUND
     lenses, mounts, cameras = set(), set(), set()
     for root in roots:
         for element in root.findall("lens"):
             lens = (name(element, "maker"), name(element, "model"), float(element.find("cropfactor").text))
             if lens in lenses:
                 print("Double primary key for lens!  {}".format(lens))
+                ERROR_FOUND = True
             else:
                 lenses.add(lens)
         for element in root.findall("mount"):
             mount = name(element, "name")
             if mount in mounts:
                 print("Double primary key for mounts!  {}".format(mount))
+                ERROR_FOUND = True
             else:
                 mounts.add(mount)
         for element in root.findall("camera"):
             camera = (name(element, "maker"), name(element, "model"), name(element, "variant"))
             if camera in cameras:
                 print("Double primary key for cameras!  {}".format(camera))
+                ERROR_FOUND = True
             else:
                 cameras.add(camera)
 
@@ -49,3 +56,8 @@ for filepath in glob.glob(os.path.join(sys.argv[1], "*.xml")):
 for check_function in [function for function in globals().copy().values()
                        if inspect.isfunction(function) and function.__name__.startswith("check_")]:
     check_function(roots)
+
+if ERROR_FOUND:
+    sys.exit(1)
+else:
+    sys.exit(0)
