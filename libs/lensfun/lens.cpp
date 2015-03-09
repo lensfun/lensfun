lensfun/lensfun

commit fadb850d85aa1d7905e57da5314ee6c1da1f056d

Made detection of lens parameters from lens model names more robust.

libs/lensfun/lens.cpp
@@ -23,6 +23,12 @@ static struct
 } lens_name_regex [] =
 {
     {
+        // [min focal]-[max focal]mm f/[min aperture]-[max aperture]
+        "[[:space:]]+([0-9]+[0-9.]*)(-[0-9]+[0-9.]*)?(mm)?[[:space:]]+(f/|f|1/|1:)?([0-9.]+)(-[0-9.]+)?",
+        { 1, 2, 5 },
+        false
+    },
+    {
         // 1:[min aperture]-[max aperture] [min focal]-[max focal]mm
         "[[:space:]]+1:([0-9.]+)(-[0-9.]+)?[[:space:]]+([0-9.]+)(-[0-9.]+)?(mm)?",
         { 3, 4, 1 },
@@ -34,12 +40,6 @@ static struct
         { 3, 4, 1 },
         false
     },
-    {
-        // [min focal]-[max focal]mm f/[min aperture]-[max aperture]
-        "([0-9]+[0-9.]*)(-[0-9]+[0-9.]*)?(mm)?[[:space:]]+(f/?)?([0-9.]+)(-[0-9.]+)?",
-        { 1, 2, 5 },
-        false
-    }
 };
 
 static float _lf_parse_float (const char *model, const regmatch_t &match)

commit c2cadad0fe2ab7580a5c529a2aade7c2d115d71c

Fixed regular expression for guessing lens parameters.
Now, "70-200mm f/2.8" is properly matched.  The problem was introduced in
fadb850.

libs/lensfun/lens.cpp
@@ -24,8 +24,8 @@ static struct
 {
     {
         // [min focal]-[max focal]mm f/[min aperture]-[max aperture]
-        "[[:space:]]+([0-9]+[0-9.]*)(-[0-9]+[0-9.]*)?(mm)?[[:space:]]+(f/|f|1/|1:)?([0-9.]+)(-[0-9.]+)?",
-        { 1, 2, 5 },
+        "([[:space:]]+|^)([0-9]+[0-9.]*)(-[0-9]+[0-9.]*)?(mm)?[[:space:]]+(f/|f|1/|1:)?([0-9.]+)(-[0-9.]+)?",
+        { 2, 3, 6 },
         false
     },
     {