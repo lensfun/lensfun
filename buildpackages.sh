eccd6617e9e28cb9c07920d4035f64643df636df
Sebastian Kraft <mail@sebastiankraft.net>
2015-11-06 21:40:05 +0100

    Improved package build script

--- buildpackages.sh
+++ buildpackages.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# remove previous build folder
+rm -rf lensfun-0.3.2
 
+# 
 git archive HEAD --prefix=lensfun-0.3.2/ -o lensfun_0.3.2.orig.tar.gz
-
 tar xvzf lensfun_0.3.2.orig.tar.gz
+
+# remove debian folder from HEAD and replace with working copy 
+rm -rf lensfun-0.3.2/debian
 cp -r debian/ lensfun-0.3.2/
+
+# build packages
 cd lensfun-0.3.2
 debuild -us -uc
 
 cd ..
-rm -rf lensfun-0.3.2
