1ae527bb932069ba91215757fa0475a703c1332f
Torsten Bronger <bronger@physik.rwth-aachen.de>
2015-12-14 21:31:58 +0100

    Added plain C interface for perspective correction.

--- libs/lensfun/mod-pc.cpp
+++ libs/lensfun/mod-pc.cpp
@@ -597,3 +597,12 @@ void lfModifier::ModifyCoord_Perspective_Correction (void *data, float *iocoord,
             iocoord [0] = iocoord [1] = 1.6e16F;
     }
 }
+
+//---------------------------// The C interface //---------------------------//
+
+cbool enable_perspective_correction (lfModifier *modifier,
+                                    float *x, float *y, int count, float d)
+{
+    fvector x_ (x, x + count), y_ (y, y + count);
+    return modifier->enable_perspective_correction (x_, y_, d);
+}
