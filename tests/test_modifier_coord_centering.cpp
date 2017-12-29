327b1fc32572d6bfb3218b5354a63817bbc7f60c
Torsten Bronger <bronger@physik.rwth-aachen.de>
2017-12-29 17:02:39 +0100

    Switched to distortion to test lens decentering.
    This way, this test can also be used in the modifier_api branch where scaling
    does not base on CenterX and CenterY anymore.

--- tests/test_modifier_coord_centering.cpp
+++ tests/test_modifier_coord_centering.cpp
@@ -25,6 +25,10 @@ typedef struct
 void mod_setup (lfFixture *lfFix, gconstpointer data)
 {
     lfFix->lens             = new lfLens();
+    lfLensCalibDistortion calib_data = {
+        LF_DIST_MODEL_POLY3, 50.0f, 50.0f, false, {-0.1}
+    };
+    lfFix->lens->AddCalibDistortion (&calib_data);
     lfFix->lens->CropFactor = 1.0f;
     lfFix->lens->AspectRatio = 3.0f / 2.0f;
     lfFix->lens->CenterX = 0.1f;
@@ -36,8 +40,8 @@ void mod_setup (lfFixture *lfFix, gconstpointer data)
 
     lfFix->mod = new lfModifier (lfFix->lens, 10.0f, lfFix->img_width, lfFix->img_height);
 
-    lfFix->mod->Initialize (lfFix->lens, LF_PF_F32, 50.89f, 2.8f, 1000.0f, 2.0f, LF_RECTILINEAR,
-                            LF_MODIFY_SCALE, true);
+    lfFix->mod->Initialize (lfFix->lens, LF_PF_F32, 50.89f, 2.8f, 1000.0f, 1.0f, LF_RECTILINEAR,
+                            LF_MODIFY_DISTORTION, true);
 
     lfFix->coordBuff = NULL;
 
@@ -53,13 +57,13 @@ void mod_teardown (lfFixture *lfFix, gconstpointer data)
     delete lfFix->lens;
 }
 
-void test_mod_coord_scaling_only (lfFixture *lfFix, gconstpointer data)
+void test_mod_coord_distortion (lfFixture *lfFix, gconstpointer data)
 {
     const float epsilon = std::numeric_limits<float>::epsilon();
-    float expected_x[] = {-1250.0f, -1050.0f, -850.000061f, -649.999939f, -450.0f,
-                          -250.000046f, -49.9999466f, 150.000015f, 349.999969f, 550.0f};
-    float expected_y[] = {-1000.0f, -800.000061f, -599.999939f, -400.0f, -200.000046f,
-                          -7.4505806e-06f, 200.000031f, 399.999969f, 600.0f, 800.0f};
+    float expected_x[] = {-9.85383224f, 92.4854813f, 194.413666f, 295.973877f, 397.20752f,
+                          498.15506f, 598.85614f, 699.348938f, 799.671326f, 899.860474f};
+    float expected_y[] = {-7.8831687f, 94.1190796f, 195.743805f, 297.033325f, 398.028809f,
+                          498.770081f, 599.296082f, 699.644897f, 799.853943f, 899.960144f};
     std::vector<float> coords (2);
     for (int i = 0; i < 10; i++)
     {
@@ -76,8 +80,8 @@ int main (int argc, char **argv)
 
   g_test_init (&argc, &argv, NULL);
 
-  g_test_add ("/modifier/coord/centering/scaling only", lfFixture, NULL,
-              mod_setup, test_mod_coord_scaling_only, mod_teardown);
+  g_test_add ("/modifier/coord/centering/distortion", lfFixture, NULL,
+              mod_setup, test_mod_coord_distortion, mod_teardown);
 
   return g_test_run();
 }
