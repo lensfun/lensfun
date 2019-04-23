8a1e2f2a71a5641c98b310747d13830dff2be2ed
Torsten Bronger <bronger@physik.rwth-aachen.de>
2019-04-23 07:14:44 +0200

    Extended calibration statistics script heavily.

--- tools/calibration_statistics/statistics.py
+++ tools/calibration_statistics/statistics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import glob, sys, os, inspect, subprocess
+import glob, sys, os, inspect, subprocess, math
 from xml.etree import ElementTree
 from pathlib import Path
 
@@ -13,8 +13,8 @@ def divide(x, y):
         return float("NaN")
 
 
-def create_distortion_plots(db_files):
-    outfile = open("distortion.dat", "w")
+def collect_distortion_data(db_files):
+    lines = []
     for filepath in db_files:
         for lens in ElementTree.parse(filepath).getroot().findall("lens"):
             calibration = lens.find("calibration")
@@ -51,13 +51,39 @@ def create_distortion_plots(db_files):
                 if max_c is None or c > max_c:
                     max_c = c
             if len(points) > 4:
+                line = []
                 for focal, inv_focal, a, b, c in points:
-                    print(divide(focal - min_f, max_f - min_f),
-                          divide(inv_focal - min_inv_f, max_inv_f - min_inv_f),
-                          divide(a - min_a, max_a - min_a),
-                          divide(b - min_b, max_b - min_b),
-                          divide(c - min_c, max_c - min_c), file=outfile)
-                print(file=outfile)
+                    line.append((divide(focal - min_f, max_f - min_f),
+                                 divide(inv_focal - min_inv_f, max_inv_f - min_inv_f),
+                                 divide(a - min_a, max_a - min_a),
+                                 divide(b - min_b, max_b - min_b),
+                                 divide(c - min_c, max_c - min_c)))
+                lines.append(line)
+    return lines
+
+
+def create_distortion_plots(data):
+    outfile = open("distortion.dat", "w")
+    for line in data:
+        for point in line:
+            print(*point, file=outfile)
+        print(file=outfile)
+
+
+def calculate_interpolation_error(data, exponents, r_max):
+    error = 0
+    for line in data:
+        for i in range(1, len(line) - 1):
+            x_l, x_0, x_r = line[i - 1][0], line[i][0], line[i + 1][0]
+            for coefficient_index in range(len(line[0]) - 2):
+                y_l, y_0, y_r = line[i - 1][coefficient_index + 1], line[i][coefficient_index + 1], \
+                    line[i + 1][coefficient_index + 1]
+                mean = y_l + (x_0 - x_l) / (x_r - x_l) * (y_r - y_l)
+                Δ = abs(y_0 - mean) * r_max ** exponents[coefficient_index]
+                if not math.isnan(Δ):
+                    error += Δ
+    return error
+
 
 try:
     rootdir = Path(sys.argv[1])
@@ -66,4 +92,6 @@ except IndexError:
 
 db_files = glob.glob(str(rootdir/"*.xml"))
 
-create_distortion_plots(db_files)
+distortion_data = collect_distortion_data(db_files)
+create_distortion_plots(distortion_data)
+print(calculate_interpolation_error(distortion_data, (4, 3, 2), 1))
