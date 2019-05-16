7868a6c4c94b93e3e9bf4a0ea00d0e598b9d7995
Christopher Degawa <ccom@randomderp.com>
2019-05-15 21:14:16 -0500

    CMake: vcpkg toolchain, gnuinstalldirs

--- cmake/modules/FindGLIB2.cmake
+++ cmake/modules/FindGLIB2.cmake
@@ -3,9 +3,6 @@ if(NOT MSVC)
     pkg_check_modules(GLIB2 QUIET glib-2.0)
 elseif(DEFINED ENV{VCPKG_ROOT})
     find_package(unofficial-glib CONFIG REQUIRED)
-    if(unofficial-glib_FOUND)
-        set(GLIB2_FOUND "${unofficial-glib_FOUND}")
-    endif()
 endif()
 
 if(WIN32 AND NOT BUILD_STATIC)
@@ -15,36 +12,30 @@ if(WIN32 AND NOT BUILD_STATIC)
         PATH_SUFFIXES glib bin)
 endif()
 
-if(NOT GLIB2_FOUND OR NOT PKG_CONFIG_FOUND)
-    find_path(GLIB2_GLIB2CONFIG_INCLUDE_PATH
-        NAMES glibconfig.h
-        HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${GLIB2_BASE_DIR}/lib ${GLIB2_BASE_DIR}/include ${CMAKE_LIBRARY_PATH}
-        PATH_SUFFIXES include glib-2.0/include)
+find_path(GLIB2_GLIB2CONFIG_INCLUDE_PATH
+    NAMES glibconfig.h
+    HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${GLIB2_BASE_DIR}/lib ${GLIB2_BASE_DIR}/include ${CMAKE_LIBRARY_PATH}
+    PATH_SUFFIXES include glib-2.0/include)
 
-    find_path(GLIB2_INCLUDE_DIRS
-        NAMES glib.h
-        HINTS /usr/local/include /usr/include /opt/local/include ${GLIB2_BASE_DIR}/include
-        PATH_SUFFIXES include gtk-2.0 glib-2.0 glib20)
+find_path(GLIB2_INCLUDE_DIRS
+    NAMES glib.h
+    HINTS /usr/local/include /usr/include /opt/local/include ${GLIB2_BASE_DIR}/include
+    PATH_SUFFIXES include gtk-2.0 glib-2.0 glib20)
 
-    find_library(GLIB2_LIBRARIES
-        NAMES  glib-2.0 glib20 glib
-        HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${GLIB2_BASE_DIR}/lib
-        PATH_SUFFIXES lib glib)
+find_library(GLIB2_LIBRARIES
+    NAMES  glib-2.0 glib20 glib
+    HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${GLIB2_BASE_DIR}/lib
+    PATH_SUFFIXES lib glib)
 
-    if(GLIB2_GLIB2CONFIG_INCLUDE_PATH AND GLIB2_INCLUDE_DIRS AND GLIB2_LIBRARIES)
-        set(GLIB2_INCLUDE_DIRS ${GLIB2_GLIB2CONFIG_INCLUDE_PATH} ${GLIB2_INCLUDE_DIRS})
-        set(GLIB2_LIBRARIES ${GLIB2_LIBRARIES})
-        set(GLIB2_FOUND 1)
-    else()
-        set(GLIB2_INCLUDE_DIRS)
-        set(GLIB2_LIBRARIES)
-        set(GLIB2_FOUND 0)
-    endif()
+if(GLIB2_GLIB2CONFIG_INCLUDE_PATH AND GLIB2_INCLUDE_DIRS AND GLIB2_LIBRARIES)
+    set(GLIB2_INCLUDE_DIRS ${GLIB2_GLIB2CONFIG_INCLUDE_PATH} ${GLIB2_INCLUDE_DIRS})
+    set(GLIB2_LIBRARIES ${GLIB2_LIBRARIES})
+    set(GLIB2_FOUND 1)
+else()
+    set(GLIB2_INCLUDE_DIRS)
+    set(GLIB2_LIBRARIES)
+    set(GLIB2_FOUND 0)
 endif()
 
 #INCLUDE( FindPackageHandleStandardArgs)
 #FIND_PACKAGE_HANDLE_STANDARD_ARGS( GLIB2 DEFAULT_MSG GLIB2_LIBRARIES GLIB2_GLIB2CONFIG_INCLUDE_PATH GLIB2_GLIB2_INCLUDE_PATH)
-
-if(NOT GLIB2_FOUND AND GLIB2_FIND_REQUIRED)
-        MESSAGE(FATAL_ERROR "Could not find glib2")
-endif()
