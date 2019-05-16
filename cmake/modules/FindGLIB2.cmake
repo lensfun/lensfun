2bcf6c64e9b40e5f2e1ff56bba62a86d0000ba45
Christopher Degawa <ccom@randomderp.com>
2019-05-15 21:04:18 -0500

    CMake: vcpkg toolchain, gnuinstalldirs

--- cmake/modules/FindGLIB2.cmake
+++ cmake/modules/FindGLIB2.cmake
@@ -1,7 +1,7 @@
 if(NOT MSVC)
-    include(FindPkgConfig)
-    pkg_search_module(GLIB2 glib-2.0)
-elseif(ENV{VCPKG_ROOT})
+    find_package(PkgConfig)
+    pkg_check_modules(GLIB2 QUIET glib-2.0)
+elseif(DEFINED ENV{VCPKG_ROOT})
     find_package(unofficial-glib CONFIG REQUIRED)
     if(unofficial-glib_FOUND)
         set(GLIB2_FOUND "${unofficial-glib_FOUND}")
@@ -10,39 +10,25 @@ endif()
 
 if(WIN32 AND NOT BUILD_STATIC)
     find_file(GLIB2_DLL
-            NAMES glib-2.dll glib-2-vs9.dll libglib-2.0-0.dll
-            PATHS ${GLIB2_LIBRARY_DIRS}/../bin
-            PATH_SUFFIXES glib bin
-            DOC "Glib2 dll")
+        NAMES glib-2.dll glib-2-vs9.dll libglib-2.0-0.dll
+        HINTS ${GLIB2_LIBRARY_DIRS}/../bin
+        PATH_SUFFIXES glib bin)
 endif()
 
 if(NOT GLIB2_FOUND OR NOT PKG_CONFIG_FOUND)
     find_path(GLIB2_GLIB2CONFIG_INCLUDE_PATH
         NAMES glibconfig.h
-        PATHS   /usr/local/lib
-                /usr/lib
-                /usr/lib64
-                /opt/local/lib
-                ${GLIB2_BASE_DIR}/lib
-                ${GLIB2_BASE_DIR}/include
-                ${CMAKE_LIBRARY_PATH}
+        HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${GLIB2_BASE_DIR}/lib ${GLIB2_BASE_DIR}/include ${CMAKE_LIBRARY_PATH}
         PATH_SUFFIXES include glib-2.0/include)
 
     find_path(GLIB2_INCLUDE_DIRS
         NAMES glib.h
-        PATHS   /usr/local/include
-                /usr/include
-                /opt/local/include
-                ${GLIB2_BASE_DIR}/include
+        HINTS /usr/local/include /usr/include /opt/local/include ${GLIB2_BASE_DIR}/include
         PATH_SUFFIXES include gtk-2.0 glib-2.0 glib20)
 
     find_library(GLIB2_LIBRARIES
         NAMES  glib-2.0 glib20 glib
-        PATHS   /usr/local/lib
-                /usr/lib
-                /usr/lib64
-                /opt/local/lib
-                ${GLIB2_BASE_DIR}/lib
+        HINTS ${CMAKE_INSTALL_FULL_LIBDIR} ${GLIB2_BASE_DIR}/lib
         PATH_SUFFIXES lib glib)
 
     if(GLIB2_GLIB2CONFIG_INCLUDE_PATH AND GLIB2_INCLUDE_DIRS AND GLIB2_LIBRARIES)
