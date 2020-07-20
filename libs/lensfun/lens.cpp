06c33c6f892ef7e770d6d948d7644e952e007a09
codingdave@gmail.com <David-Tillmann Schaefer>
2020-07-21 00:36:45 +0200

    fixes heap-buffer-overflow, #1213

--- libs/lensfun/lens.cpp
+++ libs/lensfun/lens.cpp
@@ -130,7 +130,7 @@ void lfLens::AddMount (const char *val)
 {
     if (val)
     {
-        char* p = (char*)malloc(strlen(val));
+        char* p = (char*)malloc(strlen(val) + 1);
         strcpy(p, val);
         MountNames.push_back(p);
 
--- libs/lensfun/mount.cpp
+++ libs/lensfun/mount.cpp
@@ -60,7 +60,7 @@ void lfMount::AddCompat (const char *val)
 {
     if (val)
     {
-        char* p = (char*)malloc(strlen(val));
+        char* p = (char*)malloc(strlen(val) + 1);
         strcpy(p, val);
         MountCompat.push_back(p);
 
