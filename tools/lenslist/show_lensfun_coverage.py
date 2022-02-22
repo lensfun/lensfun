56d67af18c72ced684b1446d0cb65901c1f6b08f
Šarūnas Burdulis <sarunas.burdulis@yahoo.com>
2022-02-22 11:57:57 -0500

    Adjust Markdown flavor

--- tools/lenslist/show_lensfun_coverage.py
+++ tools/lenslist/show_lensfun_coverage.py
@@ -163,7 +163,7 @@ else:
     # Markdown list
 
     if cmdline_args['table_only'] == False:
-        outfile.write("= Lensfun's coverage = \n\n == Lenses (count: {}) ==\n\n"
+        outfile.write("# Lensfun's coverage \n\n ## Lenses (count: {}) \n\n"
                   "This list was generated on {} from current Lensfun sources.  Your Lensfun version may be older, resulting in "
                   "less coverage.  \nIf your lens is not included, see \n\n* <a href='/calibration'>Upload calibration pictures</a>\n"
                   "* <a href='lens_calibration_tutorial/'>Lens calibration for Lensfun</a>\n\n".format(
@@ -177,12 +177,12 @@ else:
             number_of_makers += 1
 
         if lens.maker.lower() != previous_maker:
-            outfile.write("\n== {} ==\n\n".format(lens.maker))
+            outfile.write("\n## {}\n\n".format(lens.maker))
 
         outfile.write("* {} ({}, {}/{}/{})\n".format(lens.model, lens.crop or "?",
-            "D" if lens.distortion else "-", 
-            "T" if lens.tca else "-",
-            "V" if lens.vignetting else "-"))
+            "D" if lens.distortion else "–", 
+            "T" if lens.tca else "–",
+            "V" if lens.vignetting else "–"))
         previous_maker = lens.maker.lower()
 
 outfile.close()
