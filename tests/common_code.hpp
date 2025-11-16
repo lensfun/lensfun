8f0adb63f75049944724dbb50822dd86604fa298
Sebastian Kraft <mail@sebastiankraft.net>
2025-11-16 14:11:33 +0100

    std::aligned_alloc requires size to be a multiple of the alignment

--- tests/common_code.hpp
+++ tests/common_code.hpp
@@ -6,6 +6,13 @@ void* lf_alloc_align(size_t alignment, size_t size) {
 #ifdef _WIN32  // std::aligned_alloc isn't supported in Microsoft C Runtime library
     return _aligned_malloc(size, alignment);
 #else
+    // std::aligned_alloc requires size to be an integral multiple of alignment,
+    // otherwise it might return nullptr.
+    const size_t remainder = size % alignment;
+    if (remainder != 0) {
+        // adjust size to be multiple of alignment
+        size += alignment - remainder;
+    }
     return std::aligned_alloc(alignment, size);
 #endif
 }
